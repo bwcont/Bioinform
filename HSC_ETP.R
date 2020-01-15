16ecc74cb7e9a6f4ff1aa24cbfef5f6b4b348bb5
E.C <35582713+bwcont@users.noreply.github.com>
2020-01-15 15:47:31 -0600

    Update HSC_ETP.R

--- HSC_ETP.R
+++ HSC_ETP.R
@@ -138,7 +138,100 @@ for (i in 1:29331) {
     MLB_CB5[i,4] <- as.numeric(as.character(MLB_CB5[i,2]))
   )
 }
+#HSC
+HSC1_CB1$VALUES <- NA
+for (i in 1:29331) {
+  ifelse(
+    as.numeric(as.character(HSC1_CB1$QCpval[i]))> 0.05,
+    HSC1_CB1[i,4] <- NA, 
+    HSC1_CB1[i,4] <- as.numeric(as.character(HSC1_CB1[i,2]))
+  )
+}
 
+HSC1_CB2$VALUES <- NA
+for (i in 1:29331) {
+  ifelse(
+    as.numeric(as.character(HSC1_CB2$QCpval[i]))> 0.05,
+    HSC1_CB2[i,4] <- NA, 
+    HSC1_CB2[i,4] <- as.numeric(as.character(HSC1_CB2[i,2]))
+  )
+}
+
+HSC1_CB3$VALUES <- NA
+for (i in 1:29331) {
+  ifelse(
+    as.numeric(as.character(HSC1_CB3$QCpval[i]))> 0.05,
+    HSC1_CB3[i,4] <- NA, 
+    HSC1_CB3[i,4] <- as.numeric(as.character(HSC1_CB3[i,2]))
+  )
+}
+
+HSC1_CB4$VALUES <- NA
+for (i in 1:29331) {
+  ifelse(
+    as.numeric(as.character(HSC1_CB4$QCpval[i]))> 0.05,
+    HSC1_CB4[i,4] <- NA, 
+    HSC1_CB4[i,4] <- as.numeric(as.character(HSC1_CB4[i,2]))
+  )
+}
+
+HSC1_CB5$VALUES <- NA
+for (i in 1:29331) {
+  ifelse(
+    as.numeric(as.character(HSC1_CB5$QCpval[i]))> 0.05,
+    HSC1_CB5[i,4] <- NA, 
+    HSC1_CB5[i,4] <- as.numeric(as.character(HSC1_CB5[i,2]))
+  )
+}
+
+#Remove pvals and redundant values
+ETP_Thy1 <- ETP_Thy1[,c(-2,-3)]
+ETP_Thy2 <- ETP_Thy2[,c(-2,-3)]
+ETP_Thy3 <- ETP_Thy3[,c(-2,-3)]
+MLB_CB1 <- MLB_CB1[,c(-2,-3)]
+MLB_CB2 <- MLB_CB2[,c(-2,-3)]
+MLB_CB3 <- MLB_CB3[,c(-2,-3)]
+MLB_CB5 <- MLB_CB5[,c(-2,-3)]
+HSC1_CB1 <- HSC1_CB1[,c(-2,-3)]
+HSC1_CB2 <- HSC1_CB2[,c(-2,-3)]
+HSC1_CB3 <- HSC1_CB3[,c(-2,-3)]
+HSC1_CB4 <- HSC1_CB4[,c(-2,-3)]
+HSC1_CB5 <- HSC1_CB5[,c(-2,-3)]
+
+remove(i)
+
+### Combine into matrix to test MLPs vs ETPs#####
+mat.ETP.MLP <- cbind(HSC1_CB1[,1:2], HSC1_CB2[,2], HSC1_CB3[,2], HSC1_CB4[,2], HSC1_CB5[,2],
+                 ETP_Thy1[,2], ETP_Thy2[,2], ETP_Thy3[,2],
+                 MLB_CB1[,2], MLB_CB2[,2], MLB_CB3[,2],MLB_CB5[,2] )
+#~It is at this point i realized i mislabeled MLPs for MLBs. That's ok,
+# they are just names, so I'll rename my columns in the matrix.
+colnames(mat.ETP.MLP) <- c("GeneID","HSC1","HSC2","HSC3","HSC4","HSC5", 
+                           "ETP1", "ETP2","ETP3",
+                           "MLP1","MLP2","MLP3","MLP5")
+rownames(mat.ETP.MLP) <- 1:length(mat.ETP.MLP$GeneID)
+
+#FC calculation for each gene between EPT and MLP
+mat.ETP.MLP$FC.MLP.ETP <- NA
+
+for (z in 1:29331) {
+  numF1 <- is.na( mat.ETP.MLP[z, 7:9] ) #ETP NA
+  numL1 <- as.numeric(3 - length(numF1[numF1==TRUE])) 
+  numF2 <- is.na( mat.ETP.MLP[z, 10:13] ) #MLP NA
+  numL2 <- as.numeric(4 - length(numF2[numF2==TRUE]))
+  ETP.avg <- ( sum(mat.ETP.MLP$ETP1[z], mat.ETP.MLP$ETP2[z], mat.ETP.MLP$ETP3[z], na.rm = T) / numL1 )
+  MLP.avg <- ( sum(mat.ETP.MLP$MLP1[z], mat.ETP.MLP$MLP2[z], mat.ETP.MLP$MLP3[z], mat.ETP.MLP$MLP5[z], na.rm = T) / numL2)
+  mat.ETP.MLP$FC.MLP.ETP[z] <- ((ETP.avg - MLP.avg) / MLP.avg)
+}
+remove(numF1, numF2, z, numL1, numL2, ETP.avg, MLP.avg)
+
+#T-test calculation for each gene between EPT and MLP
+mat.ETP.MLP$pval.ETP.MLP <- NA
+for (w in 1:29331) {
+  ETP.group <- c(mat.ETP.MLP$ETP1[w], mat.ETP.MLP$ETP2[w], mat.ETP.MLP$ETP3[w]) 
+  MLP.group <- c(mat.ETP.MLP$MLP1[w], mat.ETP.MLP$MLP2[w], mat.ETP.MLP$MLP3[w], mat.ETP.MLP$MLP5[w])
+  mat.ETP.MLP$FC.MLP.ETP[w] <- ((ETP.avg - MLP.avg) / MLP.avg)
+}
 
 
 
